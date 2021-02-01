99d55539a2059dbd035633c8d682281eac906d08
Lucas Muntinga <50876764+Loikas@users.noreply.github.com>
2021-02-01 12:50:22 +0100

    Add files via upload

--- PlateChecker/Configuration.cs
+++ PlateChecker/Configuration.cs
@@ -12,12 +12,14 @@ namespace PlateCheckerRemastered
     {
         private static InitializationFile iniFile = new InitializationFile("Plugins\\LSPDFR\\PlateCheckerRemastered.ini");
         public static string PlayerName;
+        public static bool UseBritishPersona;
         public static Keys PedCheckKey;
         public static Keys PlateCheckKey;
 
         public static void ReadIniFile()
         {
             PlayerName = iniFile.ReadString("DISPLAY", "PlayerName", "Loikas");
+            UseBritishPersona = iniFile.ReadBoolean("DISPLAY", "UseBritishPersona", false);
             KeysConverter kc = new KeysConverter();
             PlateCheckKey = (Keys)kc.ConvertFromString(iniFile.ReadString("KEYBINDINGS", "PlateCheck", "C"));
             PedCheckKey = (Keys)kc.ConvertFromString(iniFile.ReadString("KEYBINDINGS", "PedCheck", "X"));
